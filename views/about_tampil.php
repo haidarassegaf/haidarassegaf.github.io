00814cb335d51584545b5b0b367bc5e67c45b6da
haidarassegaf <142563825+haidarassegaf@users.noreply.github.com>
2024-06-05 08:25:20 +0700

    Add files via upload

---
+++ views/about_tampil.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/smp.jpg"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>MANAJEMEN PERPUSTAKAAN <br> SMP AL ISLAM TAMAN</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            PERPUSTAKAAN SMP AL ISLAM TAMAN
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ views/kontak_tampil.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> Kontak</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="deskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <label class="col-sm-12 control-label"><center><strong>SMP AL ISLAM TAMAN</strong></center></label>
+                            <p align="center">
+                            Jl Ketegan Barat II gang Baik RT. 03 RW. 01<br>
+                              Kabupaten Sidoarjo, Kecamatan Taman, Kode Pos : 61257
+                            </p>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Telp. 7884512<br>
+                            Email : smpalislamtaman.kasek@dispendiksidoarjo<br>
+                            Fax : 0<br>
+                            Istagram : smp_alista_sda<br>
+                            <br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
